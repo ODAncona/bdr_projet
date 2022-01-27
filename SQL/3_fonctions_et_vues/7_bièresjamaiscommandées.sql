d85d9834faa917f42590e9fdfeb04bd74f1b4f77
herycka <eakoumba@gmail.com>
2022-01-27 19:08:01 +0100

    bière jamais commandées

---
+++ SQL/3_fonctions_et_vues/7_bièresjamaiscommandées.sql
@@ -0,0 +1,9 @@
+DROP VIEW IF EXISTS vbièresjamaiscommandées;
+CREATE OR REPLACE VIEW vbièresjamaiscommandées AS 
+SELECT Bière.nomBière,
+Bière.nomTypeBière AS "Type",
+Brasserie.nom AS "Brasserie"
+FROM Bière
+INNER JOIN Brasserie
+	ON Bière.idBrasserie = Brasserie.id
+WHERE Bière.nomBière NOT IN (SELECT nomBière FROM Commande_Bière);
