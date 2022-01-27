ae655192a1a888eda9d48ee99a6e0cae5363a2cb
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2022-01-27 19:04:41 +0100

    SQL trigger insertion bière

--- SQL/2_1_triggers/TRIGGER_before_insert_biere.sql
+++ SQL/2_1_triggers/TRIGGER_before_insert_biere.sql
@@ -1,14 +1,19 @@
+/**
+* Ajoute un nouveau type de bière si on tente d'insérer une bière
+* avec un type de bière qui n'existe pas encore.
+*/
 CREATE OR REPLACE FUNCTION fn_check_before_insert_bière()
     RETURNS TRIGGER AS
 $$
 DECLARE
     _typeBière STRING;
+    _isBrasserie INT;
 BEGIN
     SELECT INTO _typeBière nom FROM TypeBière WHERE nom = UPPER(NEW.nomTypeBière);
     IF _typeBière IS NULL THEN
         RAISE NOTICE 'Le type de bière % n''existe pas.', NEW.nomTypeBière;
         RAISE NOTICE ' -- > Ajout du type de bière %', NEW.nomTypeBière;
-        INSERT INTO TypeBière (nom) VALUES (NEW.nomTypeBière);
+        INSERT INTO TypeBière (nom) VALUES (UPPER(NEW.nomTypeBière));
     END IF;
     RETURN NEW;
 END;
