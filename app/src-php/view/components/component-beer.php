9c35a1515c1c00f90071b48c83fc1fdaffa14d95
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2022-01-28 17:55:21 +0100

    app affichage bières

--- app/src-php/view/components/component-beer.php
+++ app/src-php/view/components/component-beer.php
@@ -1,12 +1,24 @@
-<div class="container">
-<?php if (isset($beersArray)) : ?>
-  <?php foreach ($beersArray as $beer) : ?>
+<div class="container beer-container">
+<?php
+
+
+
+if (isset($beersArray)) : ?>
+  <?php foreach ($beersArray as $beer) :
+    $img = "public/images/beer_default.jpg";
+    if(isset($beer['src_images'])) {
+    //array_splice()
+    $img = 'public/images/' . $beer['src_images'];
+    //$img = $vBeer['src_images'];
+    }
+    
+    ?>
     <div class="card" style="width: 18rem;">
-      <img src="https://biereartisanale.ch/img/cms/biere.jpg" class="card-img-top" alt="...">
+      <img src="<?= $img ?>" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title"><?= $beer['nombière'] ?></h5>
         <p class="card-text"><?= $beer['description'] ?></p>
-        <a href="localhost:9999/bieres?nom=<?= $beer['nombière'] ?>&id=<?= $beer['idbrasserie']?>" class="btn btn-primary">Explore</a>
+        <a href="http://localhost:9999/bieres?nom=<?= $beer['nombière'] ?>&id=<?= $beer['idbrasserie']?>" class="btn btn-primary">Explore</a>
       </div>
     </div>
   <?php endforeach ?>
