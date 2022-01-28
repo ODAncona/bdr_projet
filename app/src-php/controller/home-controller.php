72c4fa888796f270259a7ed5d1c8043f27bb8196
Olivier D'Ancona <olivier.dancona@heig-vd.ch>
2022-01-28 20:46:14 +0100

    home-controller

--- app/src-php/controller/home-controller.php
+++ app/src-php/controller/home-controller.php
@@ -1,13 +1,70 @@
 <?php
 
-$v1 = new DBView($DB_CLIENT, 'vBière');
+// Les moins chères -----------------------------------
+$vCheap = new DBView($DB_CLIENT, 'vBière');
+$vCheap.setLimit(5);
 
 try {
     // Récupération des Bières
-    $v1->fetch();
+    $vCheap->fetch();
 } catch(PDOException $e) {
     throw $e;
 }
 
-$cheapBeers = $v1->getData();
+// Les mieux notées -----------------------------------
+$vBestMark = new DBView($DB_CLIENT, 'vBière');
+$vBestMark.setLimit(5);
+try {
+    // Récupération des Bières
+    $vBestMark->fetch();
+} catch(PDOException $e) {
+    throw $e;
+}
+
+//Les moins connues -----------------------------------
+$vLessKnown = new DBView($DB_CLIENT, 'vBière');
+$vLessKnown.setLimit(5);
+try {
+    // Récupération des Bières
+    $vLessKnown->fetch();
+} catch(PDOException $e) {
+    throw $e;
+}
+
+// Les plus commandées -----------------------------------
+$vMostOrdered = new DBView($DB_CLIENT, 'vBière');
+$vMostOrdered.setLimit(5);
+try {
+    // Récupération des Bières
+    $vMostOrdered->fetch();
+} catch(PDOException $e) {
+    throw $e;
+}
+
+// Les plus commentées -----------------------------------
+$vMostCommented = new DBView($DB_CLIENT, 'vBière');
+$vMostCommented.setLimit(5);
+try {
+    // Récupération des Bières
+    $vMostCommented->fetch();
+} catch(PDOException $e) {
+    throw $e;
+}
+
+// Les plus récentes -----------------------------------
+$vMostRecent = new DBView($DB_CLIENT, 'vBière');
+$vMostRecent.setLimit(5);
+try {
+    // Récupération des Bières
+    $vMostRecent->fetch();
+} catch(PDOException $e) {
+    throw $e;
+}
+
+$cheapBeers = $vCheap->getData();
+$bestMarkBeers = $vBestMark->getData();
+$lessKnownBeers = $vLessKnown->getData();
+$mostOrderedBeers = $vMostOrdered->getData();
+$mostCommentedBeers = $vMostCommented->getData();
+$mostRecentBeers = $vMostRecent->getData();
 require(__DIR__ . '/../view/page-home.php');
