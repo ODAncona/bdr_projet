94309167d8600e84f5507812a610be46e5abb383
Olivier D'Ancona <olivier.dancona@heig-vd.ch>
2022-01-28 07:48:07 +0100

    gen script corrigé + fichier md pointant vers dossier SQL

---
+++ conception/3_README_SQL.md
@@ -0,0 +1,5 @@
+# Modélisation SQL
+
+Nous vous invitons à naviguer sur le lien suivant afin de trouver nos scripts SQL organisés par types.
+
+[Lien vers dossier SQL](../SQL)
--- conception/4_genPDF.sh
+++ conception/4_genPDF.sh
@@ -1,5 +1,5 @@
-echo "Génération de pdf..." 
-pandoc 5_Rapport_Final.md -o 5_Rapport_Final.pdf --from markdown --template eisvogel --listings
-pandoc 5_Manuel_Utilisateur.md -o 5_Manuel_Utilisateur.pdf --from markdown --template eisvogel --listings
-pandoc 5_Guide_Installation.md -o 5_Guide_Installation.pdf --from markdown --template eisvogel --listings
+echo "Génération de pdf..."
+pandoc 4_Rapport_Final.md -o 4_Rapport_Final.pdf --from markdown --template eisvogel --listings
+pandoc 4_Manuel_Utilisateur.md -o 4_Manuel_Utilisateur.pdf --from markdown --template eisvogel --listings
+pandoc 4_Guide_Installation.md -o 4_Guide_Installation.pdf --from markdown --template eisvogel --listings
 echo "Génération terminée avec succès !"
