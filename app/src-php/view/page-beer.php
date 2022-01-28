a999cbe0eb1a954da0d29f269a87f20efb8ba19c
Olivier D'Ancona <olivier.dancona@heig-vd.ch>
2022-01-28 17:06:47 +0100

    voila

--- app/src-php/view/page-beer.php
+++ app/src-php/view/page-beer.php
@@ -7,10 +7,10 @@
 $currentPageName = $_GET['nom'];
 $filters = array();
 require("components/pagetitle.php");
-$vBeer = $data[0];
-$vAvis = $data2[0];
+$vBeer = $beers[0];
+$vAvis = $reviews[0];
 //var_dump($data);
-var_dump($vAvis)
+//var_dump($vAvis)
 ?>
 
 
@@ -23,19 +23,19 @@ var_dump($vAvis)
      <div class="col-4">
        <!-- Caractéristiques !-->
        <h2>Caractéristiques</h2>
-       <p>Type de bière: <?echo($vBeer['nomtypebière'])?></p>
-       <p>Prix Unitaire: <?echo($vBeer['prix'])?> CHF</p>
+       <p>Type de bière: <?=$vBeer['nomtypebière']?></p>
+       <p>Prix Unitaire: <?=$vBeer['prix']?> CHF</p>
 
        <!-- Note !-->
-       <h2>Note Utilisateur: <?echo($vBeer['notemoyenne'])?> / 10</h2>
-       <p>Acidité:  <?echo($vBeer['aciditemoyenne'])?>/10</p>
-       <p>Amertume:  <?echo($vBeer['amertumemoyenne'])?>/10</p>
-       <p>Douceur:  <?echo($vBeer['douceurmoyenne'])?>/10</p>
-       <p>Pétillance:  <?echo($vBeer['petillancemoyenne'])?>/10</p>
+       <h2>Note Utilisateur: <?=$vBeer['notemoyenne']?> / 10</h2>
+       <p>Acidité:  <?=$vBeer['aciditemoyenne']?>/10</p>
+       <p>Amertume:  <?=$vBeer['amertumemoyenne']?>/10</p>
+       <p>Douceur:  <?=$vBeer['douceurmoyenne']?>/10</p>
+       <p>Pétillance:  <?=$vBeer['petillancemoyenne']?>/10</p>
 
        <!-- Note !-->
       <h2>Description</h2>
-      <p> <? echo($vBeer['description'])?></p>
+      <p> <?=$vBeer['description']?></p>
      </div>
 
     <div class="col-8">
@@ -52,9 +52,9 @@ var_dump($vAvis)
 <div class="card" style="width: 18rem;">
   <img src="https://biereartisanale.ch/img/cms/biere.jpg" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title"><? echo($vBeer['nombière'])?></h5>
-    <p class="card-text"><? echo($vBeer['description'])?></p>
-    <a href="localhost:9999/bieres?nom=<?echo($vBeer['nombière'])?>&id=<?echo($vBeer['idbrasserie'])?>" class="btn btn-primary">Go somewhere</a>
+    <h5 class="card-title"><?=$vBeer['nombière']?></h5>
+    <p class="card-text"><?=$vBeer['description']?></p>
+    <a href="localhost:9999/bieres?nom=<?=$vBeer['nombière']?>&id=<?=$vBeer['idbrasserie']?>" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
 
